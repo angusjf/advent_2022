6e975f15e940475268929a1c2fd5fd3b19b15f2b
Angus Findlay <angusf@theodo.co.uk>
2022-12-02 17:15:49 +0000

    day 1 part 1 that i never commited

--- src/day01.rs
+++ src/day01.rs
@@ -1,5 +1,22 @@
 use itertools::*;
 
+fn one(input: &str) -> u32 {
+    input
+        .lines()
+        .group_by(|b| b.is_empty())
+        .into_iter()
+        .filter_map(|(empty, group)| {
+            if empty {
+                None
+            } else {
+                Some(group.map(|x| x.parse::<u32>().unwrap()))
+            }
+        })
+        .map(|x| x.sum())
+        .max()
+        .unwrap()
+}
+
 fn two(input: &str) -> u32 {
     let mut sums: Vec<_> = input
         .lines()
