7f02173d4ff14490166265f00eaab568431abb91
Angus Findlay <angus.findlay@prima.it>
2022-12-02 15:56:30 +0000

    day 2 final refactor

--- src/day02.rs
+++ src/day02.rs
@@ -1,102 +1,102 @@
-#[derive(Clone)]
+enum Result {
+    Win,
+    Lose,
+    Draw,
+}
+
+impl Result {
+    fn points(self: &Self) -> u32 {
+        match self {
+            Self::Win => 6,
+            Self::Draw => 3,
+            Self::Lose => 0,
+        }
+    }
+
+    fn from_char(c: char) -> Self {
+        match c {
+            'X' => Self::Lose,
+            'Y' => Self::Draw,
+            'Z' => Self::Win,
+            _ => unimplemented!(),
+        }
+    }
+}
+
+#[derive(Clone, PartialEq)]
 enum Move {
     Rock,
     Paper,
     Scissors,
 }
 
-enum Result {
-    Win, Lose, Draw
-}
-
 impl Move {
     fn points(self: &Self) -> u32 {
         match self {
-            Move::Rock => 1,
-            Move::Paper => 2,
-            Move::Scissors => 3,
+            Self::Rock => 1,
+            Self::Paper => 2,
+            Self::Scissors => 3,
+        }
+    }
+
+    fn defeats(self: &Self) -> Self {
+        match self {
+            Self::Rock => Self::Scissors,
+            Self::Paper => Self::Rock,
+            Self::Scissors => Self::Paper,
+        }
+    }
+
+    fn against(self: &Self, opponent: &Self) -> Result {
+        if self.defeats() == *opponent {
+            Result::Win
+        } else if opponent.defeats() == *self {
+            Result::Lose
+        } else {
+            Result::Draw
+        }
+    }
+
+    fn from_char(c: char) -> Self {
+        match c {
+            'A' => Self::Rock,
+            'X' => Self::Rock,
+            'B' => Self::Paper,
+            'Y' => Self::Paper,
+            'C' => Self::Scissors,
+            'Z' => Self::Scissors,
+            _ => unimplemented!(),
         }
     }
 }
 
-const WIN: u32 = 6;
-const DRAW: u32 = 3;
-const LOSE: u32 = 0;
-
-fn score(a: &Move, b: &Move) -> u32 {
-    match (a, b) {
-        (Move::Rock, Move::Scissors) => WIN,
-        (Move::Rock, Move::Paper) => LOSE,
-        (Move::Paper, Move::Rock) => WIN,
-        (Move::Paper, Move::Scissors) => LOSE,
-        (Move::Scissors, Move::Paper) => WIN,
-        (Move::Scissors, Move::Rock) => LOSE,
-        _ => DRAW,
-    }
-}
-
-fn parse_move(c: char) -> Move {
-    match c {
-        'A' => Move::Rock,
-        'B' => Move::Paper,
-        'C' => Move::Scissors,
-        _ => unimplemented!(),
-    }
-}
-
-fn parse_result(c: char) -> Result {
-    match c {
-        'X' => Result::Lose,
-        'Y' => Result::Draw,
-        'Z' => Result::Win,
-        _ => unimplemented!(),
-    }
-}
-
-fn parse_line(line: &str) -> (Move, Result) {
-    (
-        parse_move(line.chars().nth(0).unwrap()),
-        parse_result(line.chars().nth(2).unwrap()),
-    )
-}
-
-fn points((them, us): (&Move, &Move)) -> u32 {
-    us.points() + score(us, them)
-}
-
-fn winning_move_against(x: &Move) -> Move {
-    match x {
-        Move::Rock => Move::Paper,
-        Move::Paper => Move::Scissors,
-        Move::Scissors => Move::Rock
-    }
-}
-
-fn losing_move_against(x: &Move) -> Move {
-    match x {
-        Move::Rock => Move::Scissors,
-        Move::Paper => Move::Rock,
-        Move::Scissors => Move::Paper,
-    }
-}
-
-fn move_needed(them: &Move, result: Result) -> Move {
-    match result {
-        Result::Win => winning_move_against(them),
-        Result::Lose => losing_move_against(them),
-        Result::Draw => them.clone(),
-    }
-}
-
 fn one(input: &str) -> u32 {
-    let lines = input.lines().map(parse_line);
-        lines.map(|(them, result)| {
-            let (them, us) = (&them, move_needed(&them, result));
-            dbg!(points((&them, &us)))
+    input
+        .lines()
+        .map(|line| {
+            let their_move = Move::from_char(line.chars().nth(0).unwrap());
+            let our_move = Move::from_char(line.chars().nth(2).unwrap());
+            our_move.points() + our_move.against(&their_move).points()
+        })
+        .sum()
+}
+
+fn two(input: &str) -> u32 {
+    input
+        .lines()
+        .map(|line| {
+            let their_move = Move::from_char(line.chars().nth(0).unwrap());
+            let result = Result::from_char(line.chars().nth(2).unwrap());
+            let our_move = match result {
+                Result::Win => their_move.defeats().defeats(),
+                Result::Lose => their_move.defeats(),
+                Result::Draw => their_move.clone(),
+            };
+            our_move.points() + result.points()
         })
         .sum()
 }
 
 pub fn solve(input: &str) -> String {
-    one(input).to_string()
+    two(input).to_string()
 }
