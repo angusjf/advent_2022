937e3770622604a40dbaf2c3848549cf51ced2b8
Angus Findlay <angus.findlay@icloud.com>
2022-12-05 14:59:00 +0000

    chunksofless

--- haskell/Day05.hs
+++ haskell/Day05.hs
@@ -1,17 +1,15 @@
 import Data.List
-import Data.Sequence hiding (null, splitAt, filter, reverse)
+import Data.Sequence hiding (null, splitAt, filter, zip)
 import Data.Foldable
 import Data.Char
 
-chunks n = map toList . toList . chunksOf n . fromList
+stacks = map (filter isLetter . snd) . filter (\(i, _) -> i `mod` 4 == 0) . zip [-1..] . transpose . init
 
-stacks = map (concatMap (filter isLetter)) . transpose . init . map (chunks 4)
-      
 cmd ["move", n, "from", a, "to", b] = (read n, read a - 1, read b - 1)
 
 two stacks (n, a, b) = adjust (move ++) b $ update a keep stacks
     where (move, keep) = splitAt n $ stacks `index` a
 
-main = interact $ \input ->
-    let (start, _:end) = break null $ lines input
-    in map head $ toList $ foldl two (fromList $ stacks start) (map (cmd . words) end)
+solve (start, _:end) = foldl two (fromList $ stacks start) (map (cmd . words) end)
+
+main = interact $ map head . toList . solve . break null . lines
