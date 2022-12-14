bf8001247c0ea4fa5f425253bb8b5a453e74868a
Angus Findlay <angus.findlay@icloud.com>
2022-12-14 19:46:35 +0000

    day14 rust

---
+++ src/bin/day14.rs
@@ -0,0 +1,160 @@
+use core::{fmt, time};
+use itertools::*;
+use std::{cmp::Ordering, collections::HashSet, thread};
+
+fn parse(line: &str) -> Vec<(usize, usize)> {
+    line.split(" -> ")
+        .map(|pair| {
+            pair.split(",")
+                .map(|n| n.parse().unwrap())
+                .collect_tuple()
+                .unwrap()
+        })
+        .collect()
+}
+
+fn collides_with(path: &Vec<(usize, usize)>, (x, y): &(usize, usize)) -> bool {
+    path.iter()
+        .zip(path.iter().skip(1))
+        .any(|((x1, y1), (x2, y2))| {
+            if x1 == x2 {
+                x1 == x && ((y1..=y2).contains(&y) || (y2..=y1).contains(&y))
+            } else if y1 == y2 {
+                y1 == y && ((x1..=x2).contains(&x) || (x2..=x1).contains(&x))
+            } else {
+                unreachable!();
+            }
+        })
+}
+
+fn valid(
+    next: &(usize, usize),
+    paths: &Vec<Vec<(usize, usize)>>,
+    grains: &HashSet<(usize, usize)>,
+    max_y: usize,
+) -> bool {
+    if next.1 == max_y + 2 {
+        false
+    } else if paths.iter().any(|path| collides_with(&path, next)) {
+        false
+    } else if grains.contains(&next) {
+        false
+    } else {
+        true
+    }
+}
+
+fn print(
+    next: &(usize, usize),
+    paths: &Vec<Vec<(usize, usize)>>,
+    grains: &HashSet<(usize, usize)>,
+    min_x: usize,
+    max_x: usize,
+    max_y: usize,
+) {
+    for y in 0..=max_y + 2 {
+        for x in min_x..=max_x {
+            if next == &(x, y) {
+                print!("*");
+            } else if paths.iter().any(|path| collides_with(&path, &(x, y))) {
+                print!("#");
+            } else if grains.contains(&(x, y)) {
+                print!("o");
+            } else {
+                print!(".");
+            }
+        }
+        println!();
+    }
+    println!();
+}
+
+fn one(input: &str) -> usize {
+    // let map: Vec<Vec<char>> = vec![vec![; 1000]; 1000];
+
+    let paths: Vec<Vec<(usize, usize)>> = input.lines().map(parse).collect();
+
+    let mut grains = HashSet::new();
+
+    let max_y = *paths
+        .iter()
+        .map(|path| path.iter().map(|(_, y)| y).max().unwrap())
+        .max()
+        .unwrap();
+    let min_x = *paths
+        .iter()
+        .map(|path| path.iter().map(|(x, _)| x).min().unwrap())
+        .min()
+        .unwrap();
+    let max_x = *paths
+        .iter()
+        .map(|path| path.iter().map(|(x, _)| x).max().unwrap())
+        .max()
+        .unwrap();
+
+    'grains: loop {
+        let mut grain = (500, 0);
+
+        if !valid(&grain, &paths, &grains, max_y) {
+            break 'grains;
+        }
+
+        'grain: loop {
+            // print!("\x1B[2J\x1B[1;1H");
+            // print(&grain, &paths, &grains, min_x, max_x, max_y);
+            // thread::sleep(time::Duration::from_millis(40));
+
+            // if grain.1 > max_y {
+            // if grain.1 > max_y {
+            // break 'grains;
+            // }
+
+            {
+                let down = (grain.0, grain.1 + 1);
+                if valid(&down, &paths, &grains, max_y) {
+                    grain = down;
+                    continue;
+                }
+            }
+            {
+                // let left = (grain.0 - 1, grain.1);
+                // if valid(&left, &paths, &grains) {
+                let down_left = (grain.0 - 1, grain.1 + 1);
+                if valid(&down_left, &paths, &grains, max_y) {
+                    grain = down_left;
+                    continue;
+                }
+                // }
+            }
+            {
+                // let right = (grain.0 + 1, grain.1);
+                // if valid(&right, &paths, &grains) {
+                let down_right = (grain.0 + 1, grain.1 + 1);
+                if valid(&down_right, &paths, &grains, max_y) {
+                    grain = down_right;
+                    continue;
+                }
+                // }
+            }
+            // check if it collides with a path
+            // check if it collides with a grain
+            break 'grain;
+        }
+        // print!("\x1B[2J\x1B[1;1H");
+        // print(&grain, &paths, &grains, min_x, max_x, max_y);
+        // thread::sleep(time::Duration::from_millis(20));
+
+        grains.insert(grain);
+    }
+
+    grains.len()
+}
+
+fn two(input: &str) -> usize {
+    0
+}
+
+fn main() {
+    // println!("{}", one(include_str!("test14.txt")));
+    println!("{:?}", one(include_str!("input14.txt")));
+}
