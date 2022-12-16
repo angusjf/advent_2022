be1542f549f18973a514e67cbd7b35e797449788
Angus Findlay <angus.findlay@prima.it>
2022-12-16 19:37:58 +0000

    distances

--- src/bin/day16.rs
+++ src/bin/day16.rs
@@ -16,9 +16,43 @@ fn parse(line: &str) -> (&str, (u32, Vec<&str>)) {
     (valve, (rate, leads_to))
 }
 
-fn next_cave<'a>(cave: &'a str, valves: &'a HashMap<&'a str, (u32, Vec<&'a str>)>, visited: &'a HashSet<&'a str>) -> &'a str {
-    let (_rate, links_to) = &valves[cave];
-    links_to[0]
+fn calc_distances<'a>(valves: &'a HashMap<&'a str, (u32, Vec<&str>)>) -> HashMap<(&'a str, &'a str), u32> {
+    let mut distances: HashMap<(&str, &str), u32> = valves.iter().flat_map(|(from, (_, links_to))| {
+        links_to.iter().map(|to| ((*from, *to), 1))
+    }).collect();
+    
+    let combinations: HashSet<(&str, &str)> = valves.keys().flat_map(|from| valves.keys().map(|to| (*from, *to))).collect();
+
+    let keys = valves.keys();
+
+    for _ in 1..keys.len() {
+        combinations.iter().for_each(|(from, to)| {
+            match distances.get(&(*from, *to)) {
+                Some(_) => {}
+                None => {
+                    let dist = keys.clone().filter_map(|mid| {
+                            match distances.get(&(*from, mid)) {
+                                Some(first) => {
+                                    match distances.get(&(mid, to)) {
+                                        Some(second) => Some(first + second),
+                                        None => None
+                                    }
+                                },
+                                None => None
+                            }
+                        }).min();
+                    match dist {
+                        Some(d) => {
+                            distances.insert((*from, *to), d);
+                        },
+                        _ => {}
+                    }
+                }
+            }
+        });
+    }
+
+    distances
 }
 
 fn one(input: &str) -> u32 {
@@ -30,12 +64,22 @@ fn one(input: &str) -> u32 {
 
     let mut pressure_released: u32 = 0;
 
-    for _ in 0..30 {
+    let distances = calc_distances(&valves);
+
+    distances.iter().for_each(|((from, to), dist)| println!("{} -> {}: {}", from, to, dist));
+
+    for minuite in 0..30 {
+        println!("starting {minuite} in cave {cave}");
+
         pressure_released += visited.iter().map(|cave| valves[cave].0).sum::<u32>();
 
-        cave = next_cave(cave, &valves, &visited);
-        
-        visited.insert(cave);
+        // NEXT
+
+        let (_rate, links_to) = &valves[cave];
+
+        cave = links_to[0];
+
+        visited.insert(&cave);
     }
 
     pressure_released
@@ -54,7 +98,7 @@ fn main() {
 mod tests {
     #[test]
     fn one() {
-        assert_eq!(crate::one(include_str!("test16.txt")), 24000);
+        assert_eq!(crate::one(include_str!("test16.txt")), 1651);
     }
 
     #[test]
