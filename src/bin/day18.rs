ed798bca3de14a3300add8fb03dd735ad9c14da2
Angus Findlay <angus.findlay@icloud.com>
2022-12-18 10:43:49 +0000

    day 18 done!

--- src/bin/day18.rs
+++ src/bin/day18.rs
@@ -8,60 +8,6 @@ fn parse(line: &str) -> (i64, i64, i64) {
         .unwrap()
 }
 
-fn print(cubes: &HashSet<(i64, i64, i64)>) {
-    let mut cubes_2d: HashMap<_, _> = HashMap::new();
-    for (x, y, _) in cubes {
-        *cubes_2d.entry((*x, *y)).or_insert(0) += 1;
-    }
-
-    print_2d(&cubes_2d);
-
-    let mut cubes_2d: HashMap<_, _> = HashMap::new();
-    for (_, x, y) in cubes {
-        *cubes_2d.entry((*x, *y)).or_insert(0) += 1;
-    }
-
-    print_2d(&cubes_2d);
-
-    let mut cubes_2d: HashMap<_, _> = HashMap::new();
-    for (x, _, y) in cubes {
-        *cubes_2d.entry((*x, *y)).or_insert(0) += 1;
-    }
-
-    print_2d(&cubes_2d);
-}
-
-fn print_2d(cubes_2d: &HashMap<(i64, i64), i64>) {
-    if cubes_2d.keys().len() == 0 {
-        return;
-    }
-    let min_x = cubes_2d.keys().map(|(x, _)| x).min().unwrap();
-    let max_x = cubes_2d.keys().map(|(x, _)| x).max().unwrap();
-    let min_y = cubes_2d.keys().map(|(_, y)| y).min().unwrap();
-    let max_y = cubes_2d.keys().map(|(_, y)| y).max().unwrap();
-
-    let corner = cubes_2d.get(&(*min_x, *min_y)).unwrap();
-
-    for y in *min_y..=*max_y {
-        print!("{y:<3}|");
-        for x in *min_x..=*max_x {
-            match cubes_2d.get(&(x, y)) {
-                Some(n) => {
-                    if n == corner {
-                        print!("  ");
-                    } else {
-                        print!("{0:>2}", corner - n)
-                    }
-                }
-                None => print!("??"),
-            }
-        }
-        println!("|");
-    }
-
-    println!();
-}
-
 fn two(input: &str) -> i64 {
     let cubes: HashSet<_> = input.lines().map(parse).collect();
 
@@ -79,12 +25,9 @@ fn two(input: &str) -> i64 {
     let mut queue: VecDeque<(i64, i64, i64)> = VecDeque::from([(min_x, min_y, min_z)]);
 
     while let Some(pos @ (x, y, z)) = queue.pop_back() {
-        if (x >= min_x)
-            && (y >= min_y)
-            && (z >= min_z)
-            && (x <= max_x)
-            && (y <= max_y)
-            && (z <= max_z)
+        if (min_x..=max_x).contains(&x)
+            && (min_y..=max_y).contains(&y)
+            && (min_z..=max_z).contains(&z)
         {
             if cubes.contains(&pos) {
                 lava += 1;
@@ -97,7 +40,7 @@ fn two(input: &str) -> i64 {
                     (0, 1, 0),
                     (0, -1, 0),
                     (1, 0, 0),
-                    (1, 0, 0),
+                    (-1, 0, 0),
                 ]
                 .iter()
                 .for_each(|(dx, dy, dz)| queue.push_front((x + dx, y + dy, z + dz)));
